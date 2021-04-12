86e283c6c39b9dccea8e566554e04e2a93d4603e
miguelmejiaq <miguelmejiaq@outlook.com>
2021-04-12 18:38:30 -0500

    Added card view and Onboarding view

---
+++ Fructus/Data/FruitData.swift
@@ -0,0 +1,20 @@
+//
+//  FruitData.swift
+//  Fructus
+//
+//  Created by Miguel Angel Mejia Quiroga on 12/04/21.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA
+
+let fruitsData: [Fruit] = [
+    Fruit(
+        title: "Blueberry",
+        headline: "Blueberries are sweet, nutriuous and widly popular fruit all over the world.",
+        image: "blueberrt",
+        gradientColor: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")],
+        description: "Blueberries are perennial flowering plants with blue or purple berries.",
+        nutrition: ["240 KJ","9.96 g", "0.33 g", "0.74 g", "A, B1", "Calcium, Magnesium"])
+]
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,37 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Miguel Angel Mejia Quiroga on 12/04/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    // MARK: - BODY
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack (spacing: 8) {
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical,10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25 )
+            )
+        } //: BUTTON
+        .accentColor(Color.white)
+    }
+}
+// MARK: - VIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
