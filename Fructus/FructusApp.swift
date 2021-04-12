af86128c64baee55daf279405b94485746c52049
miguelmejiaq <miguelmejiaq@outlook.com>
2021-04-12 17:22:42 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Miguel Angel Mejia Quiroga on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
