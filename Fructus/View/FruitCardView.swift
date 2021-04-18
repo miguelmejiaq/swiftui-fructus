53e725e8b307a82a4042bb6ad61b4fa5d1b4951f
esteban-alo <esrodriguezalo@gmail.com>
2021-04-17 22:06:09 -0500

    Fixed overlapped button

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -22,6 +22,8 @@ struct FruitCardView: View {
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y:8)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: 2396/2, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 937.5/2, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
                 // FRUIT: TITLE
                 Text(fruit.title)
                     .foregroundColor(Color.white)
@@ -53,6 +55,6 @@ struct FruitCardView: View {
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
         FruitCardView(fruit: fruitsData[1])
-            .previewLayout(.fixed(width: 320, height: 640))
+            .previewLayout(.fixed(width: 2436, height: 1125))
     }
 }
